b06b872460dba3c26267cf521830355472b488ac
sajeebmahamed <sajeebahamed6@gmail.com>
2022-10-30 19:12:04 +0600

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-interface IMyInterface {
-    myProperty: string;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const myInterface: IMyInterface = {
-    myProperty: 'myValue'
-}
-export default myInterface;
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
